742034816fa97d760e27a5d319ee571a881eb01c
claudevanes <dvorskakamila@gmail.com>
2016-05-17 15:14:31 +0200

    First commit.

---
+++ src/sas/Main.java
@@ -0,0 +1,13 @@
+package sas;
+
+public class Main {
+
+	public Main() {
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/ses/RenameMe.java
@@ -0,0 +1,9 @@
+package ses;
+
+public class RenameMe {
+
+	public RenameMe() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
